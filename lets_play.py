dd76a95c17451af7a3261abc9c881c1728b5cd33
Akshay23504 <aaravind@syr.edu>
2019-03-06 21:54:05 -0500

    Write to excel file working

--- lets_play.py
+++ lets_play.py
@@ -5,13 +5,17 @@ import pandas as pd
 class ExcelPlayTime:
     def __init__(self):
         self.df = pd.DataFrame(columns=["Column1", "Column2", "Column3", "Column4", "Column5"])
-        self.sample_data = ["Data1", "Data2", "Data3", "Data4", "Data5"]
+        self.sample_data_row1 = ["Data11", "Data12", "Data13", "Data14", "Data15"]
+        self.sample_data_row2 = ["Data21", "Data22", "Data23", "Data24", "Data25"]
+        self.sample_data_row3 = ["Data31", "Data32", "Data33", "Data34", "Data35"]
 
     def write_to_excel(self):
         data_frame = self.df
-        data_frame.loc["Row1"] = self.sample_data
-        data_frame.loc["Row2"] = self.sample_data
-        data_frame.loc["Row3"] = self.sample_data
+        data_frame.loc["Row1"] = self.sample_data_row1
+        data_frame.loc["Row2"] = self.sample_data_row2
+        data_frame.loc["Row3"] = self.sample_data_row3
         data_frame.to_csv("write_file.csv")
 
 
+excel_play_time = ExcelPlayTime()
+excel_play_time.write_to_excel()
